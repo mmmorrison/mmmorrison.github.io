8149b839b24b2f4a78c5d0dd501354169892cc1a
Maria <mmorrison1978@gmail.com>
2016-04-17 18:54:10 -0600

    added jquery

--- main.js
+++ main.js
@@ -55,6 +55,16 @@
 }( document, window, 0 ));
 
 
+// Floating label headings for the contact form
+$(function() {$("body")
+ .on("input propertychange", ".form-item",function(e) {
+  $(this).toggleClass("form-item-filled",!! $(e.target).val());})
+ .on("focus", ".form-item",function() {
+  $(this).addClass("form-item-focused");})
+ .on("blur", ".form-item",function() {
+  $(this).removeClass("form-item-focused");});
+});
+
 // Highlight the top nav as scrolling occurs
 $('body').scrollspy({target: '.navbar-fixed-top'})
 
@@ -69,3 +79,5 @@ $(function() {$('body').on('click', 'a.scrollable', function(event) {
   event.preventDefault();
   });
 });
+
+	
